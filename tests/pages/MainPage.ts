e916745a5c3ed0f894b4d978731130be218e0b4a
tyavinamua <tyavinamua@63pokupki.ru>
2025-08-16 13:49:10 +0400

    fix добавлено  ожидание

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,8 @@
 import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
-export class MainPage extends BasePage{
+export class MainPage extends BasePage {
  async open() {
-    this.page.goto('https://rutube.ru');
+    await this.page.goto('https://rutube.ru');
  }
 }
 
